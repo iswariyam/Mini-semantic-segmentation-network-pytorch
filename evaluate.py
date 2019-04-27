13e3ea953132b493fa7e886bced735e849f1cc13
iswariyam <iswariya.m17@gmail.com>
2019-04-28 00:29:52 +0200

    Linting code

--- evaluate.py
+++ evaluate.py
@@ -5,14 +5,15 @@ Created on Mon Sep  3 14:13:47 2018
 
 @author: iswariya
 """
-from metric import Metric
 import numpy as np
 import torch
 
+from metric import Metric
+
 
 def predict(model, testloader, criterion, **kwargs):
-    '''Function to evaluate the model
-    
+    """Function to evaluate the model
+
     Parameters
     ----------
     model : object
@@ -21,7 +22,7 @@ def predict(model, testloader, criterion, **kwargs):
         iter object for loading data from test set
     criterion : pytorch loss module
         Contains the loss function for optimizing the model
-        
+
     Returns
     -------
     List
@@ -30,37 +31,33 @@ def predict(model, testloader, criterion, **kwargs):
         List of annotated mask tensors from the dataset
     Tuple
         List of iou per class and mean iou
-        
-    '''
-    
+
+    """
+
     model.eval()
     metric_val = Metric(4)
     metric_val.reset()
     running_loss = 0
-    
+
     device = kwargs['device']
     batch_size = kwargs['batchsize']
     prediction_array = np.ndarray((batch_size, 120, 160))
     mask_array = np.ndarray((batch_size, 120, 160))
 
-    
     for i, data in enumerate(testloader):
-        
+
         images = data['image'].to(device)
         masks = data['mask'].to(device)
-        
+
         output = model(images)
-        
         loss = criterion(output, masks)
-        
-        running_loss += loss.item()
-        
+
         pred = torch.argmax(output, dim=1)
         iou = metric_val.get_iou(pred, masks)
-        
-        prediction_array = np.vstack((prediction_array, pred.detach().cpu().numpy()))
-        #print(masks.detach().cpu().numpy().shape, mask_array.shape)
+        prediction_array = np.vstack((prediction_array,
+                                     pred.detach().cpu().numpy()))
+        # print(masks.detach().cpu().numpy().shape, mask_array.shape)
         mask_array = np.vstack((mask_array, masks.detach().cpu().numpy()))
-        #print(prediction_array.shape, mask_array.shape)
+        # print(prediction_array.shape, mask_array.shape)
 
     return prediction_array, mask_array, iou, metric_val.confusion_matrix
